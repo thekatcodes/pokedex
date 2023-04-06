d1497fb25cb99df9bf0e2e4d7c4650e1e728bb32
Katie Liu <katieliu59@gmail.com>
2023-04-05 22:18:38 -0400

    Update search code, implement useEffect hook

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Navbar(props) {
+	const { fetchPokemon } = props;
 
-    const { searchValue, searchInput } = props;
+	const [searchValue, setSearchValue] = useState("");
 
-    return (
-        <>
-		<input
-			className="search-field"
-			type="search"
-			value={searchValue}
-			placeholder="Search Pokémon"
-			onChange={(event) => searchInput(event)}
-            ></input>
-        </>
+	// Set searchValue state everytime the input field changes
+	const searchInput = (event) => {
+		setSearchValue(event.target.value);
+	};
+
+	// useEffect listens for changes to the searchValue state, when it changes, it calls the fetchPokemon function with the new searchValue
+	useEffect(() => {
+		fetchPokemon(searchValue);
+	}, [searchValue]);
+
+	return (
+		<>
+			<input
+				className="search-field"
+				type="search"
+				value={searchValue}
+				placeholder="Search Pokémon"
+				onChange={(event) => searchInput(event)}
+			></input>
+		</>
 	);
 }
