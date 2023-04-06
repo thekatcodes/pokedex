4846554bd007b090557261d5cb27486281f78c7b
Katie Liu <katieliu59@gmail.com>
2023-04-05 23:56:39 -0400

    Update code with logic to display data

--- src/pages/PokemonCard.js
+++ src/pages/PokemonCard.js
@@ -1,9 +1,26 @@
 import React from "react";
 
-export default function PokemonCard() {
-    return (
+export default function PokemonCard(props) {
+	const { pokemonData } = props;
 
-        <h1>this is the PokemonCard</h1>
-    )
-    
+	console.log("POKEMONCARD", pokemonData);
+
+	if (pokemonData.length === 20) {
+        return pokemonData.map((pokemon) => {
+			return (
+				<div>
+					<h3>{pokemon.name}</h3>
+				</div>
+			);
+		});	} else {
+		return pokemonData.map((pokemon) => {
+			return (
+				<div>
+					<h3>#{pokemon.id}</h3>
+					<img src={pokemon.sprites.other.home.front_default} />
+					<h3>{pokemon.name}</h3>
+				</div>
+			);
+		});
+	}
 }
