2997fd6031736d94aa02077a9c227d1625cd0d1f
Katie Liu <katieliu59@gmail.com>
2023-04-06 00:50:41 -0400

    Refactor and clean code to display pokemon information

--- src/pages/PokemonCard.js
+++ src/pages/PokemonCard.js
@@ -5,17 +5,10 @@ export default function PokemonCard(props) {
 
 	console.log("POKEMONCARD", pokemonData);
 
-	if (pokemonData.length === 20) {
-        return pokemonData.map((pokemon) => {
-			return (
-				<div>
-					<h3>{pokemon.name}</h3>
-				</div>
-			);
-		});	} else {
+	if (pokemonData.length > 0) {
 		return pokemonData.map((pokemon) => {
 			return (
-				<div>
+				<div key={pokemon.id}>
 					<h3>#{pokemon.id}</h3>
 					<img src={pokemon.sprites.other.home.front_default} />
 					<h3>{pokemon.name}</h3>
