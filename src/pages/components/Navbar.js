caf33b7e68ef48eefa586df5af9091273122e153
Katie Liu <katieliu59@gmail.com>
2023-04-08 00:37:01 -0400

    Refactor code to use Image component

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Navbar(props) {
 	const { fetchPokemon } = props;
@@ -16,9 +17,16 @@ export default function Navbar(props) {
 	}, [searchValue]);
 
 	return (
-		<div className="navbar text-center">
+		<div className="navbar text-center flex mt-4 mb-12">
+			<Image
+				src="/logo.png"
+				alt="Pokemon Logo"
+				width={100}
+                height={100}
+                className='navbar__logo ml-4'
+			/>
 			<input
-				className="navbar__search border-solid border-2 border-sky-500 px-4 py-3 mt-4 mb-4 rounded-full"
+				className="navbar__search border-solid border-2 border-sky-500 px-4 py-3 mx-auto rounded-full"
 				type="search"
 				value={searchValue}
 				placeholder="Search Pokémon"
