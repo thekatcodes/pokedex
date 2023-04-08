bb526880afee1995cf28530c3d97e45afabb4862
Katie Liu <katieliu59@gmail.com>
2023-04-07 22:42:03 -0400

    Update pokemon cards display style

--- src/pages/PokemonCard.js
+++ src/pages/PokemonCard.js
@@ -7,20 +7,20 @@ export default function PokemonCard(props) {
 
     if (pokemonData.length > 0) {
         return (
-          <div className="grid grid-cols-4 gap-4">
+          <div className="grid grid-cols-4 gap-20">
             {pokemonData.map((pokemon) => (
               <div
-                className="relative max-w-sm rounded overflow-visible shadow-lg h-20"
+                className="relative max-w-sm rounded overflow-visible shadow-lg h-20 mt-6 mb-6"
                 key={pokemon.id}
               >
-                <div className="absolute top-0 left-0 w-1/2 h-full overflow-visible">
+                <div className="absolute -top-5 -left-6 w-1/2 h-full overflow-visible">
                   <img
-                    className="absolute top-0 left-0 z-10 h-28"
+                    className="absolute -top-5 -left-6 z-10 h-30"
                     src={pokemon.sprites.other.home.front_default}
                   />
                 </div>
-                <h3 className="z-0">#{pokemon.id}</h3>
-                <h3 className="z-0">{pokemon.name}</h3>
+                <h3 className="z-0 text-center ml-5">#{pokemon.id}</h3>
+                <h3 className="z-0 text-center ml-5">{pokemon.name}</h3>
               </div>
             ))}
           </div>
