97b8d902538e39917b1f1b6692c88602f7df99e4
Katie Liu <katieliu59@gmail.com>
2023-04-08 16:30:27 -0400

    Refactor code

--- src/pages/PokemonCard.js
+++ src/pages/PokemonCard.js
@@ -8,33 +8,37 @@ export default function PokemonCard(props) {
 
 	if (pokemonData.length > 0) {
 		return (
-			<div className="pokemon-cards grid grid-cols-4 gap-20">
-				{pokemonData.map((pokemon) => (
-					<div
-						className="relative max-w-sm border-solid border-2 border-grey-100 rounded-2xl overflow-visible shadow-lg h-20 mt-6 mb-6 flex items-center justify-around"
-						key={pokemon.id}
-					>
-						<div className="absolute -top-4 -left-6 w-1/2 h-full overflow-visible">
-							<img
-								className="absolute -top-4 -left-6 z-10 h-30"
-								src={pokemon.sprites.other.home.front_default}
-							/>
+			<>
+				<div className="pokemon-cards grid grid-cols-4 gap-20">
+					{pokemonData.map((pokemon) => (
+						<div
+							className="relative max-w-sm border-solid border-2 border-grey-100 rounded-2xl overflow-visible shadow-lg h-20 mt-6 mb-6 flex items-center justify-around"
+							key={pokemon.id}
+						>
+							<div className="absolute -top-4 -left-6 w-1/2 h-full overflow-visible">
+								<img
+									className="absolute -top-4 -left-6 z-10 h-30"
+									src={pokemon.sprites.other.home.front_default}
+								/>
+							</div>
+							<div className="pokemon-cards__name flex flex-col">
+								<h3 className="z-0 text-center ml-5">
+									#
+									{pokemon.id < 10
+										? `00${pokemon.id}`
+										: pokemon.id <= 99
+										? `0${pokemon.id}`
+										: pokemon.id}
+								</h3>
+								<h3 className="z-0 uppercase text-center ml-5">
+									{pokemon.name}
+								</h3>
+							</div>
 						</div>
-						<div className="pokemon-cards__name flex flex-col">
-							<h3 className="z-0 text-center ml-5">
-								#
-								{pokemon.id < 10
-									? `00${pokemon.id}`
-									: pokemon.id <= 99
-									? `0${pokemon.id}`
-									: pokemon.id}
-							</h3>
-							<h3 className="z-0 uppercase text-center ml-5">{pokemon.name}</h3>
-						</div>
-					</div>
-                ))}
-                <Modal />
-			</div>
+					))}
+				</div>
+				<Modal />
+			</>
 		);
 	}
 }
