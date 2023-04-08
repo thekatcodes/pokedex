5c040d800a2c09c7e199ba4e4319e679dafa056d
Katie Liu <katieliu59@gmail.com>
2023-04-07 22:16:02 -0400

    Organize pokemon list in grid of 4

--- src/pages/PokemonCard.js
+++ src/pages/PokemonCard.js
@@ -5,20 +5,25 @@ export default function PokemonCard(props) {
 
 	console.log("POKEMONCARD", pokemonData);
 
-	if (pokemonData.length > 0) {
-		return pokemonData.map((pokemon) => {
-			return (
-				<div
-					className="relative max-w-sm rounded overflow-visible shadow-lg h-20"
-					key={pokemon.id}
-				>
-					<div className="absolute top-0 left-0 w-1/2 h-full overflow-visible">
-						<img className="absolute top-0 left-0 z-10 h-28" src={pokemon.sprites.other.home.front_default} />
-					</div>
-					<h3 className="z-0">#{pokemon.id}</h3>
-					<h3 className="z-0">{pokemon.name}</h3>
-				</div>
-			);
-		});
-	}
-}
+    if (pokemonData.length > 0) {
+        return (
+          <div className="grid grid-cols-4 gap-4">
+            {pokemonData.map((pokemon) => (
+              <div
+                className="relative max-w-sm rounded overflow-visible shadow-lg h-20"
+                key={pokemon.id}
+              >
+                <div className="absolute top-0 left-0 w-1/2 h-full overflow-visible">
+                  <img
+                    className="absolute top-0 left-0 z-10 h-28"
+                    src={pokemon.sprites.other.home.front_default}
+                  />
+                </div>
+                <h3 className="z-0">#{pokemon.id}</h3>
+                <h3 className="z-0">{pokemon.name}</h3>
+              </div>
+            ))}
+          </div>
+        );
+      }
+    }
