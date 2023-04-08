149d48a42195e146dfa51347d04c5db6ff01a378
Katie Liu <katieliu59@gmail.com>
2023-04-08 19:43:03 -0400

    Implement modal functionality and styling

--- src/pages/components/Modal.js
+++ src/pages/components/Modal.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-export default function Modal() {
-    return (
+export default function Modal(props) {
+	const { showModal, onClose } = props;
 
-        <h1>this is the Modal</h1>
-    )
-    
+	if (!showModal) return null;
+
+	return (
+		<div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+			<div className="w-[600px] flex flex-col">
+				<button
+					className="text-white text-xl place-self-end"
+					onClick={() => onClose()}
+				>
+					Close
+				</button>
+				<div className="bg-white p-2 rounded">MODAL</div>
+			</div>
+		</div>
+	);
 }
