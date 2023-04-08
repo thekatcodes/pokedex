f588ce40aa76b5540d37aac49f43a734a45c19f0
Katie Liu <katieliu59@gmail.com>
2023-04-07 23:15:38 -0400

    Update classNames to BEM methodology

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -9,21 +9,21 @@ export default function Navbar(props) {
 	const searchInput = (event) => {
 		setSearchValue(event.target.value);
 	};
-console.log(searchValue)
+	console.log(searchValue);
 	// useEffect listens for changes to the searchValue state, when it changes, it calls the fetchPokemon function with the new searchValue
 	useEffect(() => {
 		fetchPokemon(searchValue);
 	}, [searchValue]);
 
 	return (
-		<>
+		<div className="navbar text-center">
 			<input
-				className="search-field"
+				className="navbar__search border-solid border-2 border-sky-500 px-4 py-3 mt-4 mb-4 rounded-full"
 				type="search"
 				value={searchValue}
 				placeholder="Search Pokémon"
 				onChange={(event) => searchInput(event)}
 			></input>
-		</>
+		</div>
 	);
 }
