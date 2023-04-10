2ad7a61bfa4979bb14ab20f5ecb07b5232497531
Katie Liu <katieliu59@gmail.com>
2023-04-10 02:49:04 -0400

    Implement logic to render progress bar for each stat, make code cleaner

--- src/pages/components/Modal.js
+++ src/pages/components/Modal.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Image from "next/image";
 import { pokemonType, colorClass } from "../../utils/helper";
 
 export default function Modal(props) {
 	const { pokemon, showModal, onClose } = props;
+	const [containerWidth, setContainerWidth] = useState(0);
 	console.log(pokemon);
+
+	// Get pixel width of the container with useRef, useEffect hook  and offsetWidth
+	const containerRef = useRef(null);
+
+	useEffect(() => {
+		if (showModal) {
+			setContainerWidth(containerRef.current.offsetWidth);
+		}
+	}, [showModal]);
 
 	if (!showModal) {
 		return null;
 	}
 
+	// type variable contains the first type of the pokemon
 	const type = pokemon[0].types[0].type.name;
 	pokemonType(type);
 	console.log(pokemon[0].types[0].type.name);
 
+	// let progressHP = containerWidth * pokemon[0].stats[0].base_stat/255
+	// console.log('PROGRESS', progressHP)
+	console.log("CONTAINER AFTER", containerWidth);
+
+	console.log(
+		"MATH",
+		Math.floor((containerWidth * pokemon[0].stats[0].base_stat) / 255)
+	);
 	return (
 		<div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
 			<div className="w-[500px] flex flex-col">
@@ -38,7 +57,7 @@ export default function Modal(props) {
 					<img
 						src={pokemon[0].sprites.other["official-artwork"].front_default}
 						alt={pokemon[0].name}
-						className=""
+						width={100}
 					/>
 					<div className="bg-grey-100 p-3 m-2 rounded-2xl ">
 						<div className="flex justify-center mt-20 mb-3">
@@ -93,28 +112,77 @@ export default function Modal(props) {
 								<p>Abilities</p>
 							</div>
 						</div>
-                        <h4 className="flex justify-center">Base Stats</h4>
-                        {/* Calls pokemonType function again to set the default color to the first type (if pokemon has more than 1 type) */}
-                        {pokemonType(type)} 
-                        <div>
-							<p>HP {pokemon[0].stats[0].base_stat}</p>
-                            <div class={`mt-2 ${colorClass} rounded-full bg-opacity-25`}>
-								<div class={`w-[78px] mt-2 ${colorClass} py-0 rounded-full opacity-100`}>
-									<div class=" text-white text-sm inline-block bg-purple-700 px-2 rounded-full">
+						<h4 className="flex justify-center">Base Stats</h4>
+						{/* Calls pokemonType function again to set the default color to the first type (if pokemon has more than 1 type) */}
+						{pokemonType(type)}
+						{/* <div>
+							<p>HP</p>
+							<div
+								ref={containerRef}
+								className={`mt-2 ${colorClass} rounded-full bg-opacity-25`}
+							>
+								<div
+									className={`w-[${Math.floor(
+										(containerWidth * pokemon[0].stats[0].base_stat) / 255
+									)}px] mt-2 ${colorClass} py-0 rounded-full opacity-100`}
+								>
+									<div className=" text-white text-sm inline-block bg-purple-700 px-2 rounded-full">
 										{pokemon[0].stats[0].base_stat}
 									</div>
 								</div>
 							</div>
 							<p>ATK {pokemon[0].stats[1].base_stat}</p>
+							<div
+								ref={containerRef}
+								className={`mt-2 ${colorClass} rounded-full bg-opacity-25`}
+							>
+								<div
+									className={`w-[${Math.floor(
+										(containerWidth * pokemon[0].stats[1].base_stat) / 255
+									)}px] mt-2 ${colorClass} py-0 rounded-full opacity-100`}
+								>
+									<div className=" text-white text-sm inline-block bg-purple-700 px-2 rounded-full">
+										{pokemon[0].stats[1].base_stat}
+									</div>
+								</div>
+							</div>
 							<p>DEF {pokemon[0].stats[2].base_stat}</p>
 							<p>SATK {pokemon[0].stats[3].base_stat}</p>
 							<p>SDEF {pokemon[0].stats[4].base_stat}</p>
-							<p>SPD {pokemon[0].stats[5].base_stat}</p>
-						</div>
-						
+							<p>SPD {pokemon[0].stats[5].base_stat}</p> */}
+
+						{pokemon[0].stats.map((stat) => {
+							console.log(
+								"BOOmath",
+								Math.floor((containerWidth * stat.base_stat) / 255)
+							);
+                            console.log("boo", containerWidth);
+                            
+                                
+
+							return (
+								<div className="flex justify-center">
+									<p>{stat.stat.name}</p>
+									<div
+										ref={containerRef}
+										className={` flex-grow mt-2 ${colorClass} rounded-full bg-opacity-25`}
+									>
+										<div
+											className={`mt-2 ${colorClass} py-0 rounded-full opacity-100`}
+										    style={{ width: `${Math.floor(containerWidth * stat.base_stat / 255)}px`}}
+                                        >
+											<div className=" text-white text-sm inline-block bg-purple-700 px-2 rounded-full">
+												{stat.base_stat}
+											</div>
+										</div>
+									</div>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
 		</div>
+		// </div>
 	);
 }
