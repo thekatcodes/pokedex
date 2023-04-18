7a33a809e42430db808f289b0afcac847436cf78
Katie Liu <katieliu59@gmail.com>
2023-04-18 02:34:33 -0400

    Comment out code

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
 <footer className="flex justify-center align-end">     
-    <p>Project developed by Katie Liu, and open-sourced</p>
+    {/* <p>Project developed by Katie Liu, and open-sourced</p> */}
 </footer>
     )
     
